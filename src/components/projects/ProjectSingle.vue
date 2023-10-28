09f1665ef449c1e435bae6563c04c29c85c13d9b
A3Techstack <149150528+A3Techstack@users.noreply.github.com>
2023-10-28 03:39:54 +0100

    Update ProjectSingle.vue

--- src/components/projects/ProjectSingle.vue
+++ src/components/projects/ProjectSingle.vue
@@ -30,7 +30,7 @@ export default {
 				target="_blank"
 				class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light hover:underline"
 			>
-				Project Link
+				Click to Preview
 			</a>
 			<!-- <span
 				class="font-general-medium text-lg text-ternary-dark dark:text-ternary-light"
